2049c4edcb1b90f6f44426a0e00fe9297b292f0e
chinargupta <chinar4778.be22@chitkara.edu.in>
2025-07-22 20:00:12 +0530

    cylinder

---
+++ level1/cylinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Cylinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter radius of the cylinder: ");
+        double radius = scanner.nextDouble();
+
+        System.out.print("Enter height of the cylinder: ");
+        double height = scanner.nextDouble();
+
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+
+        System.out.printf("Volume of the cylinder: %.2f\n", volume);
+
+        scanner.close();
+    }
+}
